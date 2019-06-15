906719afe3d9834fc805ee8022fab26ccd8acafd
Tomás Barcellos <tomasbarcellos@gmail.com>
2019-06-15 12:20:40 -0300

    Incluí primeiras visualizações da aba inicial

--- app.R
+++ app.R
@@ -1,5 +1,31 @@
 library(shiny)
 library(shinydashboard)
+library(tidyverse)
+library(tidytext)
+library(lexiconPT)
+
+options(scipen = 9999)
+
+theme_set(theme_classic())
+
+ler_tuites <- function(path) {
+  env <- new.env()
+  load(path, env)
+  env <- as.list(env)
+  do.call(bind_rows, env) %>% 
+    as_tibble()
+}
+
+tuites <- dir("data/", full.names = TRUE) %>% 
+  str_subset("usuarios", TRUE) %>% 
+  map_df(ler_tuites) %>% 
+  distinct()
+
+data("oplexicon_v3.0")
+
+tidy_tuites <- tuites %>% 
+  unnest_tokens(word, text) %>% 
+  left_join(lexiconPT::oplexicon_v3.0, by = c("word" = "term"))
 
 ui <- dashboardPage(
   dashboardHeader(),
@@ -56,23 +82,54 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
   output$graf_evo_tuites <- renderPlot({
-    plot(1, 1, "n")
-    text(1, 1, "twittes")
+    tuites %>% 
+      mutate(data = lubridate::round_date(created, "minute")) %>% 
+      count(term, data) %>% 
+      ggplot(aes(data, n, col = term)) +
+      geom_line() +
+      theme(legend.position = "top",  legend.direction = "horizontal", 
+            legend.title = element_blank())
   })
   
   output$graf_evo_sentimento <- renderPlot({
-    plot(1, 1, "n")
-    text(1, 1, "Sentimento")
+    tidy_tuites %>% 
+      mutate(data = lubridate::round_date(created, "minute")) %>% 
+      group_by(term, data) %>% 
+      summarise(sentimento = sum(polarity, na.rm = TRUE)) %>% 
+      ggplot(aes(data, sentimento, col = term)) +
+      geom_line() +
+      theme(legend.position = "top",  legend.direction = "horizontal", 
+            legend.title = element_blank())
   })
   
   output$graf_retuites <- renderPlot({
-    plot(1, 1, "n")
-    text(1, 1, "retuites")
+    tuites %>%
+      group_by(term) %>%
+      summarise(tot = sum(retweetCount)) %>%
+      filter(tot >= 10 ^ 5) %>% 
+      ggplot(aes(x = reorder(term, tot), tot)) +
+      geom_bar(stat = "identity", fill = "forestgreen") +
+      geom_text(aes(x = reorder(term,tot), y = 10 ^7, 
+                    label = paste0(signif(tot/10 ^ 6,2)," Milhões") ),
+                size = 2.8) +
+      xlab("Termos") +
+      ylab("Retweets") +
+      coord_flip()
   })
   
   output$graf_pessoas <- renderPlot({
-    plot(1, 1, "n")
-    text(1, 1, "pessoas")
+    tuites %>%
+      group_by(usuario = screenName) %>%
+      summarise(tot = sum(retweetCount)) %>%
+      top_n(10) %>% 
+      ggplot(aes(x = reorder(usuario, tot), tot)) +
+      geom_bar(stat = "identity", fill = "forestgreen") +
+      geom_text(aes(x = reorder(usuario, tot), y = 1.5 * (10 ^ 4), 
+                    label = paste0(signif(tot/10 ^ 3,2)," Mil") ),
+                size = 2.8) +
+      xlab("Usuário") +
+      ylab("Retweets") +
+      coord_flip() 
   })
   
   output$graf_rede_relacoes <- renderPlot({
