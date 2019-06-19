18c3368c9b08857c8775c4c1a68efc604860d539
cndesantana <charles.santana@gmail.com>
2019-06-19 07:20:40 +0200

    Raspador 3 sera o raspador automatico a ser usado em conjunto com o cron

--- raspador3.R
+++ raspador3.R
@@ -6,15 +6,9 @@ library(dplyr)
 library(ggplot2)
 library(lubridate)
 
-
-#test <- identical(token, get_token())
-#
-#if(!test){
-#   exit(1)
-#}
-## get user IDs of accounts followed by CNN
-tmls <- search_tweets("#ShowDoPavão", n = 20000)
-#tmls <- get_timelines(c("TheInterceptBr", "SF_Moro", "deltanmd", "LulaOficial"), n = 3200)
-#tmls %>% filter(as.Date(format(created_at,"%Y-%m-%d")) >= as.Date("2019-05-01")) %>% group_by(screen_name) %>% summarise(totRet = sum(retweet_count))
-save(tmls, file = "data/tweets_hashtag_pavao.RData")
-
+data <- system("date +\"%Y%m%d%H%M\"") 
+termos <- c("#VazaJato")
+for(termo in termos){
+   tmls <- search_tweets(termo, n = 20000)
+   save(tmls, file = paste0("data/tweets_",data,".RData"))
+}
