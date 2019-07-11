c974ee1124dfea3a196b9081c37e7c9558dbd898
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2019-07-11 11:27:13 -0300

    Investigações posteriores nos influenciadores

--- Rede_de_Palavras/trata_cluster.R
+++ Rede_de_Palavras/trata_cluster.R
@@ -35,7 +35,7 @@ df_trabalho<- df_termo_user %>%
 
 cont<<-0
 
-df_cluster<- map_dfr(unique(df_trabalho$termo), function(termo_q){
+df_trabalho<- map_dfr(unique(df_trabalho$termo), function(termo_q){
   
 
   
@@ -55,8 +55,10 @@ df_cluster<- map_dfr(unique(df_trabalho$termo), function(termo_q){
       return()
     }
     
-    influencers<- unlist(df_tl$mentions_screen_name)
-    tibble( nome=rep(user_t,length(influencers)), influencers= influencers)
+    #influencers<- unlist(df_tl$mentions_screen_name)
+    #tibble( nome=rep(user_t,length(influencers)), influencers= influencers)
+    
+    df_tl
     
     
   })
@@ -65,14 +67,33 @@ df_cluster<- map_dfr(unique(df_trabalho$termo), function(termo_q){
   df_influencers
 })
 
-quantile((df_cluster %>%
-        filter(!is.na(influencers)) %>%
-  group_by(termo, influencers) %>%
-  summarise(
-    contagem = n()
 
-  ))$contagem,0.75)
+
+#df_trabalho <- df_cluster
+
+
+cont<<-0
+df_influencers <- map_dfr(df_trabalho$status_id,function(status){
   
+  cont<<-cont+1
+  
+  print(status)
+  print((cont/NROW(df_trabalho))*100)
+  
+  
+  influencers <- unlist((df_trabalho%>% filter(status_id == status) %>% select(mentions_screen_name))$mentions_screen_name)
+  tibble( status=rep(status,length(influencers)), influencers= influencers)
+  
+})
+
+glimpse(df_influencers)
+
+names(df_influencers)[1] <- "status_id"
+
+df_cluster<- df_influencers %>%
+  inner_join(df_trabalho)
+
+df_cluster_copy <- df_cluster
 
 
 
@@ -87,7 +108,7 @@ df_factor_influencers<-
     contagem = n()
     
   ) %>%
-  #arrange(desc(contagem))%>%
+  arrange(desc(contagem))%>%
   mutate(influencers=fct_reorder(influencers,contagem))
 
 factor(data$Carreira, levels = data$Carreira[order(data$Valor)])
@@ -107,23 +128,31 @@ df_factor_influencers<- df_cluster %>%
 
 
 #As duas hastags
-df_cluster %>%
+top_influencers<- df_cluster %>%
   filter(!is.na(influencers)) %>% 
-  group_by(termo, influencers) %>%
+  group_by(influencers) %>%
     summarise(
       contagem = n()
     ) %>%
     arrange(influencers) %>%
-  top_n(20) %>%
-   ggplot(aes(x= influencers, y=contagem)) +
-  geom_col(fill= "lightblue") +
+  top_n(20)
+
+df_cluster %>%
+  filter(influencers %in% top_influencers$influencers) %>%
+  group_by(termo, influencers) %>%
+  summarise(
+    contagem = n()
+  ) %>%
+  arrange(influencers) %>%
+   ggplot(aes(x= influencers, y=contagem, fill=termo)) +
+  geom_col() +
   facet_grid(termo ~ .) +
   theme_minimal() +
   theme(
     axis.text.x = element_text(angle = 90)
   )
 
- 
+
 #Somente hashtag #eutocommoro
 df_cluster %>%
   filter(!is.na(influencers),
@@ -165,10 +194,40 @@ df_cluster %>%
 save(list = "df_cluster", file = "~/GitHub/vazajato/Rede_de_Palavras/data/df_cluster.RData")
 
 
+df_cluster %>%
+  filter(influencers %in% top_influencers$influencers) %>%
+  mutate(tipo_mencao = ifelse(is.na(reply_to_screen_name),"Retuíte",ifelse(reply_to_screen_name==influencers,"Resposta","Adição"))) %>%
+  #filter(tipo_mencao == "Retuíte")%>%
+  group_by(termo, influencers, tipo_mencao) %>%
+  ggplot(aes(x= influencers, fill= tipo_mencao)) +
+  geom_bar() +
+  facet_grid(termo ~ .) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 90)
+  )
+
+df_cluster %>%
+  filter(influencers %in% top_influencers$influencers) %>%
+  mutate(tipo_mencao = ifelse(is.na(reply_to_screen_name),"Retuíte",ifelse(reply_to_screen_name==influencers,"Resposta","Adição"))) %>%
+  filter(tipo_mencao == "Retuíte")%>%
+  group_by(termo, influencers, tipo_mencao) %>%
+  ggplot(aes(x= influencers, fill= termo)) +
+  geom_bar() +
+  #facet_grid(termo ~ .) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 90)
+  )
+
+
 
 #1113875475905036291
 #	726769395436019713
 #3029339182
 df_tl_quali<- try(rtweet::get_timeline("3029339182"))
 df_tl_quali_res <- df_tl_quali %>%
-                select(screen_name, status_id,mentions_screen_name, )
+                   filter(!is.na(mentions_screen_name))
+  
+  
+                select(screen_name, status_id,mentions_screen_name, text)
