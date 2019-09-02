bd8a4205534a45739043a92beb36a760f22cc75c
tarssioesa <tarssio.disap@hotmail.com>
2019-09-02 18:20:05 -0300

    cpqn_data

--- R/raspador.R
+++ R/raspador.R
@@ -20,10 +20,10 @@ library(readxl)
 library(httr)
 library(twitteR)
 
-api_key = "XXXXXXXXXXXXXXX"
-api_secret = "YYYYYYYYYYYYYYYYYYYYYYYYYY"
-access_token = "XYXYXYXYXYXYXXYXYXYXYXYYXYX"
-access_token_secret = "ZZZZZZZZZZZZZZZZZZZZZZZZ"
+api_key = "iY1RjU0xu7Ym3zT5fipT26Nx6"
+api_secret = "F9f6pjDKmMEGv4bpHp1NY84DspZrUiboXZ5SVUYZ93orHdh2iu"
+access_token = "1000032762064199680-0z6tbJp5LvSjv7WFzdCeS34iuW9AtK"
+access_token_secret = "1mA6vTNP3qWKiC1dHoUBXBdFBVLS8ybksbkiSiNtey3OP"
 
 setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 
@@ -35,17 +35,21 @@ if(length(args) > 2)
    datefim <- args[2]
    terms <- args[-c(1,2)]
 }else{
-   terms <-c("#VazaJato","Intercept","Moro","Dallagnol","#MoroCriminoso")
-   dateini <-c("2019-06-09")
-   datefim <-c("2019-06-10")
+   terms <-c("cnpq","CNPQ", "Marcos Pontes","Future-se","Abraham Weintraub",
+             "Weintraub", "@Astro_Pontes", "@AbrahamWeint")
+   dateini <-c("2019-08-26")
+   datefim <-c("2019-09-02")
 }
 
 df_tweets <-data.frame()
-for(i in 5:length(terms)){
+for(i in 1:7){
   term <- terms[i]
-  tweets <-searchTwitter(term, n=10000, lang="pt-br",dateini,datefim,resultType = "recent")
+  tweets <-searchTwitter(term, n=10000, lang="pt-br",dateini,
+                         datefim)
   df_tweets <- rbind(df_tweets,cbind(twListToDF(tweets),term))
 }
 
-save(df_tweets,file="df_tweets_10000_MoroCriminoso_vazajato.Rdat")
+
+
+save(df_tweets,file="df_pesquisa.Rdat")
 
